4bf5729a1f7c580799393c1356f1598dffbc5a56
Monkey457 <FRCteam457@gmail.com>
2024-02-28 17:55:09 -0600

    Comment for roboInit code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -32,6 +32,8 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+
+    // Basic robot camera initializations for teleoperation and autonomous modes--planning on using an april tag recognition program under auto.
     m_robotContainer = new RobotContainer();
     UsbCamera camera = CameraServer.startAutomaticCapture();
 
